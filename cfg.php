1b9d3e4ee9a1fcbd4173ae9d948adc1bc29b40b5
Bayu Riza alfarizi <32736177+devikamenor@users.noreply.github.com>
2019-08-23 23:20:49 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,4 @@
+<?php
+
+$data = '{"articleid":"130933","userid":"1070623","Infversion":"1.0","Key":"0nVV4xX+YudkJ6d69OWQFhbY7O12zNFABTU/rDQtu7TUUmgyd6pl6Q==","Method":"ArticleValidReadReward","UID":"1566553807822","appv":"3.43","country":"Indonesia","i18n":"id_ID","sourceOrigin":"GooglePlay","systemtype":"2"}';
+
